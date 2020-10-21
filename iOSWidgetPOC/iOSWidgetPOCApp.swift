6a7e18e45ffb26f81f17bdfaa85106a149831b39
Juan Bustillo <juan.bustillo@unosquare.com>
2020-10-21 16:38:51 -0500

    Initial swift ui commit

---
+++ iOSWidgetPOC/iOSWidgetPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSWidgetPOCApp.swift
+//  iOSWidgetPOC
+//
+//  Created by Juan Bustillo Dev on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct iOSWidgetPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
